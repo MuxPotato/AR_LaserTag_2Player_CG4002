976e13c20d5c8f47dda30404e668d3c50dd322d7
Wang Haoyang <haoyangw@gmail.com>
2024-09-09 16:20:28 +0800

    Add basic functions     Helper functions to connect to Bluno Beetles via BLE

--- connect.py
+++ connect.py
@@ -1,5 +1,12 @@
 from bluepy.btle import Scanner, DefaultDelegate
 
+# Parameters
+BLUNO_NAME = 'Bluno'
+BLUNO_MANUFACTURER_ID = "4c000215e2c56db5dffb48d2b060d0f5a71096e000000000c5"
+BLUNO_MAC_ADDR_LIST = [
+    "f4:b8:5e:42:67:2b"
+]
+
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -10,11 +17,45 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print("Received new data from: {}".format(dev.addr))
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(5.0)
+def parseDeviceName(nameValue):
+    # Bluno pads its 'Complete Local Name' with trailing '\x00' characters, so we strip them
+    return nameValue.rstrip('\x00').strip()
 
-for dev in devices:
-    print("Device {} ({}), RSSI={} dB".format(dev.addr, dev.addrType, dev.rssi))
-    for (adtype, desc, value) in dev.getScanData():
-        print("  {} = {}".format(desc, value))
-        print("Value text: {}".format(dev.getValueText(adtype)))
+def fetchCurrentDevices(period = 5.0):
+    scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(period)
+    return devices
+
+def scanAllDevices():
+    """ scanner = Scanner().withDelegate(ScanDelegate())
+    devices = scanner.scan(5.0) """
+    devices = fetchCurrentDevices(5.0)
+
+    for dev in devices:
+        """ print("Device {} ({}), RSSI={} dB".format(dev.addr, dev.addrType, dev.rssi))
+        for (adtype, desc, value) in dev.getScanData():
+            print("  {} = {}".format(desc, value))
+            print("Value text: {}".format(dev.getValueText(adtype))) """
+        for (adtype, desc, value) in dev.getScanData():
+            if adtype == 9 and parseDeviceName(value) == BLUNO_NAME: # 'Complete Local Name' is 'Bluno'
+                print("Found Bluno device with MAC: {}".format(dev.addr))
+
+def hasBluno(devices):
+    for dev in devices:
+        if dev in BLUNO_MAC_ADDR_LIST:
+            return True
+    return False
+
+def getBlunoFrom(devices):
+    blunos = []
+    for dev in devices:
+        if dev in BLUNO_MAC_ADDR_LIST:
+            blunos.append(dev)
+    return blunos
+
+def connectTo(mac_addr):
+
+devices = fetchCurrentDevices(5.0)
+blunos = getBlunoFrom(devices)
+if (len(blonos) > 0):
+    for bluno in blunos

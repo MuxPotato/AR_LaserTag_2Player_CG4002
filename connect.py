df71c59a1db320503a9ddbdb9f0ccdf89c501894
Wang Haoyang <haoyangw@gmail.com>
2024-09-09 14:52:28 +0800

    Update Python syntax in connect.py

--- connect.py
+++ connect.py
@@ -6,14 +6,15 @@ class ScanDelegate(DefaultDelegate):
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
-            print "Discovered device", dev.addr
+            print("Discovered device: {}".format(dev.addr))
         elif isNewData:
-            print "Received new data from", dev.addr
+            print("Received new data from: {}".format(dev.addr))
 
 scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
+devices = scanner.scan(5.0)
 
 for dev in devices:
-    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+    print("Device {} ({}), RSSI={} dB".format(dev.addr, dev.addrType, dev.rssi))
     for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+        print("  {} = {}".format(desc, value))
+        print("Value text: {}".format(dev.getValueText(adtype)))

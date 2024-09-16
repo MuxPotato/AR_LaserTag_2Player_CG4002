6d1b9e8f14a61740fb57992e6ee4021b8fad92fc
Wang Haoyang <haoyangw@gmail.com>
2024-09-17 01:32:30 +0800

    Major code improvements -Implement CRC byte for dummy packets -Implement function to generate generic packets -Implement function in preparation for 3 way handshake

--- ble/ble.ino
+++ ble/ble.ino
@@ -1,10 +1,21 @@
 #include "packet.h"
+#include "CRC8.h"
 
 bool doHandshake();
 
 bool hasHandshake = false;
 uint16_t seqNum = 0;
 String receiveBuffer = "";
+
+void createPacket(BlePacket &packet, byte packetType, short seqNum, byte data[16]) {
+  packet.metadata = packetType;
+  packet.packetId = seqNum;
+  for (byte i = 0; i < 16; i += 1) {
+    packet.data[i] = data[i];
+  }
+  // TODO: Implement proper checksum
+  packet.checksum = 1;
+}
 
 void sendDummyPacket() {
   BlePacket dummyPacket;
@@ -15,11 +26,31 @@ void sendDummyPacket() {
   dummyPacket.data[2] = (byte)'M';
   dummyPacket.data[3] = (byte)'M';
   dummyPacket.data[4] = (byte)'Y';
-  dummyPacket.checksum = 1;
+  //dummyPacket.checksum = 1;
+  CRC8 crcGen;
+  crcGen.add((uint8_t) dummyPacket.metadata);
+  crcGen.add((uint8_t) dummyPacket.packetId);
+  for (auto c : dummyPacket.data) {
+    crcGen.add((uint8_t) c);
+  }
+  dummyPacket.checksum = crcGen.calc();
   Serial.write((byte *) &dummyPacket, sizeof(dummyPacket));
 }
 
 void sendAckPacket() {
+  BlePacket ackPacket;
+  ackPacket.metadata = 1;
+  ackPacket.packetId = 0;
+  ackPacket.data[0] = (byte)'A';
+  ackPacket.data[1] = (byte)'C';
+  ackPacket.data[2] = (byte)'K';
+  ackPacket.data[3] = 0;
+  ackPacket.data[4] = 0;
+  ackPacket.checksum = 1;
+  Serial.write((byte *) &ackPacket, sizeof(ackPacket));
+}
+
+void sendSynPacket(byte seqNum) {
   BlePacket ackPacket;
   ackPacket.metadata = 1;
   ackPacket.packetId = 0;
@@ -44,15 +75,11 @@ void setup() {
 * Beetle for some reason
  */
 void loop() {
-  // Create a receive buffer
-  String HELLO = "HELLOxxxxxxxxxxxxxxx";
   if (Serial.available()) {
     char newByte = Serial.read();
     // Append new byte to receive buffer
     receiveBuffer += newByte;
     // ACK complete packet
-    /* This IF block is not working. The buffer isn't doing anything */
-    // if (receiveBuffer.length() >= 20) {
     if (receiveBuffer.length() >= PACKET_SIZE) {
       String curr = receiveBuffer.substring(0, PACKET_SIZE);
       receiveBuffer.remove(0, PACKET_SIZE);
@@ -132,6 +159,7 @@ void loop() {
  * 
  * Returns: boolean where true signifies successful handshake and false signifies failed handshake
  */
+/*
 bool doHandshake() {
   while (!Serial.available());
   // Data received, check whether it's a HELLO packet
@@ -160,4 +188,22 @@ bool doHandshake() {
   // }
   // return inputs[0] == packetIds::ACK;
   return true;
+}
+*/
+bool doHandshake() {
+  while (!Serial.available());
+  String helloBuffer = "";
+  // Check for HELLO packet
+  while (Serial.available()) {
+    char nextByte = Serial.read();
+    helloBuffer += nextByte;
+    if (helloBuffer.length() == 20) {
+      // We have received one complete packet, stop reading BLE data
+      break;
+    }
+  }
+  
+  // Send SYN packet
+
+  // Check for SYN+ACK packet
 }

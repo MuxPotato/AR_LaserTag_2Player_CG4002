c9ec918d073311555da08e1be2aa17aef554fad8
PoobalanAatmikaLakshmi <aatmikalakshmi123@gmail.com>
2024-09-16 02:29:21 +0000

    adding color

--- RelayServer.py
+++ RelayServer.py
@@ -2,7 +2,7 @@ from threading import Thread,Event
 import queue 
 import time 
 import socket 
-
+from Color import print_message
 
 
 class RelayServer(Thread): 
@@ -58,9 +58,11 @@ class RelayServer(Thread):
             client.close()  # Ensure the connection is closed after handling
     
     def processMessage(self,msg):
-        print("RelayServer: Sending message to AI") 
+        print_message('Relay Server',"Sending message to AI")
+        #print("RelayServer: Sending message to AI") 
         self.IMU_queue.put(msg)
-        print("RelayServer: Sending message to game engine")
+        print_message('Relay Server',"Sending message to game engine")
+        #print("RelayServer: Sending message to game engine")
         print("-" * 30)
         self.game_engine_queue.put(msg)
         
@@ -68,12 +70,14 @@ class RelayServer(Thread):
     def run(self): 
         self.server.listen(1)
         # time.sleep(2)
-        print(f"Relay Server is listening on {self.host}:{self.port}")
+        print_message('Relay Server',f'listening on {self.host}:{self.port}')
+        #print(f"Relay Server is listening on {self.host}:{self.port}")
         try:
             while not self.stop_event.is_set():
                 try:
                     client, address = self.server.accept()
-                    print(f"Relay Client connected from {address}")
+                    print_message('Relay Server',f"Relay Client connected from {address}")
+                    #print(f"Relay Client connected from {address}")
                     self.handleClient(client, address)
                 except socket.timeout:
                     continue 
@@ -81,7 +85,6 @@ class RelayServer(Thread):
             self.server.close()  # Ensure the server is closed when exiting
             
     def shutdown(self):
-        """Gracefully shutdown the relay server."""
         self.stop_event.set()  # Set the stop event to stop the server loop
         self.server.close()  # Close the server socket
         print("Relay server shutdown initiated")

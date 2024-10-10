829aa0a74d394af1944b847c4cace827146551e5
Wang Haoyang <haoyangw@gmail.com>
2024-10-11 00:11:27 +0800

    Refactor external comms code -Update code to send packets to RelayServer instead of dumping in terminal

--- external_main.py
+++ external_main.py
@@ -8,18 +8,22 @@ from internal_main import InternalMainThread
 from internal_utils import bcolors
 from relay_client import RelayClient
 
+# Constants
+SERVER_IP = "172.26.191.210"
+SERVER_PORT = "5055"
+
 if __name__=="__main__":
     outgoing_glove_queue = queue.Queue()
     outgoing_game_state_queue = queue.Queue()
     incoming_game_state_queue = queue.Queue()
     int_main = InternalMainThread(outgoing_glove_queue,outgoing_game_state_queue,incoming_game_state_queue)
-    output_dir = "output/"
+#    output_dir = "output/"
     glove_output_filename = read_user_input("Enter the filename to dump glove data to: ")
     game_state_output_filename = read_user_input("Enter the filename to dump game state data to: ")
-    # relay_client = RelayClient(outgoing_glove_queue, outgoing_game_state_queue)
+    relay_client = RelayClient(SERVER_IP, SERVER_PORT, outgoing_glove_queue, outgoing_game_state_queue)
     # TODO: Remove 2 lines below
-    relay_client = RelayClient(outgoing_glove_queue, outgoing_game_state_queue,
-            f"""{output_dir}/{glove_output_filename}""", f"""{output_dir}/{game_state_output_filename}""")
+#    relay_client = RelayClient(outgoing_glove_queue, outgoing_game_state_queue,
+#            f"""{output_dir}/{glove_output_filename}""", f"""{output_dir}/{game_state_output_filename}""")
     try:
         int_main.start()
         relay_client.start()
--- relay_client.py
+++ relay_client.py
@@ -6,70 +6,75 @@ import sys
 import threading
 import traceback
 
-from external_utils import QUEUE_GET_TIMEOUT, write_packet_to
+from external_utils import QUEUE_GET_TIMEOUT
 
-def handle_IMU_data(terminate_event, from_ble_IMU_queue, glove_output):
+def handle_IMU_data(terminate_event, from_ble_IMU_queue, send_func):
     while not terminate_event.is_set():
         try:
             IMU_data = from_ble_IMU_queue.get(timeout = QUEUE_GET_TIMEOUT)  
             print(f'IMU data received from int comms: {IMU_data}')
             # Here, you would parse the data and send it to the server
-            # player_data = self.parse_packets(IMU_data, ShootPacket())  # Pass dummy ShootPacket if needed
-            # self.send(IMU_data)
+            send_func(IMU_data)
 
-            # TODO: Remove line below
-            write_packet_to(glove_output, IMU_data)
         except queue.Empty:
             continue
         except Exception as exc:
             traceback.print_exception(exc)
 
 
-def handle_shoot_data(terminate_event, from_ble_shoot_queue, game_output):
+def handle_shoot_data(terminate_event, from_ble_shoot_queue, send_func):
     while not terminate_event.is_set():
         try:
             shoot_data = from_ble_shoot_queue.get(timeout = QUEUE_GET_TIMEOUT)  
             print(f'Shoot data received from int comms: {shoot_data}')
-            # player_data = self.parse_packets(IMUPacket(), shoot_data)  # Pass dummy IMUPacket if needed
-            # self.send(player_data)
+            send_func(shoot_data)
 
-            # TODO: Remove line below
-            write_packet_to(game_output, shoot_data)
         except queue.Empty:
             continue
         except Exception as exc:
             traceback.print_exception(exc)
 
+def get_send_func(socket):
+    def send_func(given_msg):
+        message = json.dumps(given_msg)
+        length = str(len(message))
+        first = length + "_"
+        socket.sendall(first.encode("utf-8"))
+        socket.sendall(message.encode("utf-8"))
+        print(f'Sent {message} to relay server')
+    
+    return send_func
+
+# Function to parse the IMU and gun/vest packets into a unified dictionary
+def parse_packets(imu_packet, Shootpacket):
+    # Create a unified dictionary for the player
+    player_data = {
+        'playerID': imu_packet.playerID,
+        'accel': imu_packet.accel,
+        'gyro': imu_packet.gyro,
+        'isFire': Shootpacket.isFire,
+        'isHit': Shootpacket.isHit
+    }
+    
+    return player_data
+
 class RelayClient(threading.Thread):
 
-#    def __init__(self, server_ip, server_port, from_ble_IMU_queue, from_ble_shoot_queue):
-    def __init__(self, from_ble_IMU_queue, from_ble_shoot_queue, glove_output, game_output):
+    def __init__(self, server_ip, server_port, from_ble_IMU_queue, from_ble_shoot_queue):
+#    def __init__(self, from_ble_IMU_queue, from_ble_shoot_queue, glove_output, game_output):
         super().__init__()
-        """ self.server_ip = server_ip
-        self.server_port = server_port   """
+        self.server_ip = server_ip
+        self.server_port = server_port  
         self.timeout = 100   # the timeout for receiving any data
         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.from_ble_IMU_queue = from_ble_IMU_queue
         self.from_ble_shoot_queue = from_ble_shoot_queue 
         self.stop_event = threading.Event()
         self.imu_thread = threading.Thread(target=handle_IMU_data,
-                args = (self.stop_event, from_ble_IMU_queue, glove_output,))
+                args = (self.stop_event, from_ble_IMU_queue, get_send_func(self.socket),))
         self.shoot_thread = threading.Thread(target=handle_shoot_data, 
-                args = (self.stop_event, from_ble_shoot_queue, game_output,))
+                args = (self.stop_event, from_ble_shoot_queue, get_send_func(self.socket),))
         # Define the NamedTuple structures
-
-# Function to parse the IMU and gun/vest packets into a unified dictionary
-    def parse_packets(imu_packet, Shootpacket):
-        # Create a unified dictionary for the player
-        player_data = {
-            'playerID': imu_packet.playerID,
-            'accel': imu_packet.accel,
-            'gyro': imu_packet.gyro,
-            'isFire': Shootpacket.isFire,
-            'isHit': Shootpacket.isHit
-        }
-        
-        return player_data
 
     def connect(self,host,port):
         try:
@@ -90,8 +95,8 @@ class RelayClient(threading.Thread):
         pass
 
     def run(self):
-        #self.connect(self.server_ip,self.server_port)
-        #print('Connected to relay server')
+        self.connect(self.server_ip,self.server_port)
+        print('Connected to relay server')
         try:
             self.imu_thread.start()
             self.shoot_thread.start()
@@ -106,5 +111,5 @@ class RelayClient(threading.Thread):
         self.imu_thread.join()
         self.shoot_thread.join()
         print('Threads for IMU and Shoot data stopped')
-        #self.socket.close()
+        self.socket.close()
         print('Relay client terminated')

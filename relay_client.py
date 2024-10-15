4c7d8e47398ef794992045ce57d6eda975bbf15d
Wang Haoyang <haoyangw@gmail.com>
2024-10-15 17:24:32 +0800

    Add seperate Thread for receiving game state update -Wait for game state update from RelayServer as a separate Thread on the RelayClient

--- relay_client.py
+++ relay_client.py
@@ -8,6 +8,67 @@ import traceback
 
 from external_utils import QUEUE_GET_TIMEOUT
 
+class ReceiverThread(threading.Thread):
+    def __init__(self, my_socket: socket.SocketType, server_ip: str, stop_event: threading.Event, to_ble_game_state_queue: queue.Queue):
+        self.my_socket = my_socket
+        self.server_ip = server_ip
+        self.stop_event = stop_event
+        self.to_ble_game_state_queue = to_ble_game_state_queue
+        
+    def handle_receive(self):
+        try:
+            # Receive length followed by '_' followed by message
+            data = b''
+            while not data.endswith(b'_'):
+                _d = self.my_socket.recv(1)
+                if not _d:  
+                    print(f"Server {self.server_ip} disconnected")
+                    raise ConnectionResetError("Server disconnected")
+                data += _d
+            
+            if len(data) == 0:
+                print("No data received")
+                return None
+
+            data = data.decode("utf-8")
+            length = int(data[:-1])
+
+            data = b''
+            while len(data) < length:
+                _d = self.my_socket.recv(length - len(data))
+                if not _d: 
+                    print(f"Server {self.server_ip} disconnected")
+                    raise ConnectionResetError("Server disconnected")
+                data += _d
+
+            if len(data) == 0:
+                print("No data received")
+                return None
+
+            received_msg = data.decode("utf-8")
+            if length != len(data):
+                print("Packet length does not match, packet dropped")
+                return None
+            else:
+                print(f"Received message from server: {received_msg}")
+                return received_msg 
+
+        except (ConnectionResetError, socket.error) as e:
+            print(f"Error: {str(e)}. Connection might be lost.")
+            return None
+        
+    def update_game_state(self, received_msg: str):
+        # game state is a dictionary
+        game_state = json.loads(received_msg)
+        self.to_ble_game_state_queue.put(game_state)
+
+    def run(self):
+        while not self.stop_event.is_set():
+            received_msg = self.handle_receive()
+            if received_msg is not None:
+                # Do something with the received message
+                self.update_game_state(received_msg)  
+
 def handle_IMU_data(terminate_event, from_ble_IMU_queue, send_func):
     while not terminate_event.is_set():
         try:
@@ -62,7 +123,7 @@ def parse_packets(imu_packet, Shootpacket):
 
 class RelayClient(threading.Thread):
 
-    def __init__(self, server_ip, server_port, from_ble_IMU_queue, from_ble_shoot_queue):
+    def __init__(self, server_ip, server_port, from_ble_IMU_queue, from_ble_shoot_queue, to_ble_game_state_queue):
 #    def __init__(self, from_ble_IMU_queue, from_ble_shoot_queue, glove_output, game_output):
         super().__init__()
         self.server_ip = server_ip
@@ -71,12 +132,14 @@ class RelayClient(threading.Thread):
         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.from_ble_IMU_queue = from_ble_IMU_queue
         self.from_ble_shoot_queue = from_ble_shoot_queue 
+        self.to_ble_game_state_queue = to_ble_game_state_queue
         self.stop_event = threading.Event()
         self.imu_thread = threading.Thread(target=handle_IMU_data,
                 args = (self.stop_event, from_ble_IMU_queue, get_send_func(self.socket),))
         self.shoot_thread = threading.Thread(target=handle_shoot_data, 
                 args = (self.stop_event, from_ble_shoot_queue, get_send_func(self.socket),))
-        # Define the NamedTuple structures
+        self.receive_handler = ReceiverThread(self.socket, self.server_ip, self.stop_event, 
+                self.to_ble_game_state_queue)
 
     def connect(self,host,port):
         try:
@@ -142,6 +205,8 @@ class RelayClient(threading.Thread):
             self.imu_thread.start()
             self.shoot_thread.start()
             print('Threads for IMU and Shoot data started')
+            self.receive_handler.start()
+            print('Thread for receiving data from RelayServer started')
             self.stop_event.wait()
         except Exception as exc:
             traceback.print_exception(exc)
@@ -151,6 +216,7 @@ class RelayClient(threading.Thread):
         self.stop_event.set()
         self.imu_thread.join()
         self.shoot_thread.join()
-        print('Threads for IMU and Shoot data stopped')
+        self.receive_handler.join()
+        print('Threads for IMU, Shoot data and receiving data stopped')
         self.socket.close()
         print('Relay client terminated')

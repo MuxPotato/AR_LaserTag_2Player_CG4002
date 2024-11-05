cad59f78a49a0b76be089f3625e5cef101b06970
Wang Haoyang <haoyangw@gmail.com>
2024-11-05 21:43:06 +0800

    Improve BleDelegate -Only put data received from the Serial BLE GATT Characteristic in the Beetle class input buffer -Drop all data received from other BLE GATT Characteristics for now

--- ble_delegate.py
+++ ble_delegate.py
@@ -2,7 +2,7 @@ import traceback
 import bluepy
 from bluepy.btle import DefaultDelegate
 
-from internal_utils import BAUDRATE_SETUP, BLUNO_GATT_PASSWORD, LOWER_4BITS_MASK, PACKET_SIZE, BlePacketType, bcolors, is_metadata_byte
+from internal_utils import BAUDRATE_SETUP, BLUNO_GATT_PASSWORD, GATT_SERIAL_CHARACTERISTIC_HANDLE, LOWER_4BITS_MASK, PACKET_SIZE, BlePacketType, bcolors, is_metadata_byte
 
 # Delegate
 class BlePacketDelegate(DefaultDelegate):
@@ -14,15 +14,18 @@ class BlePacketDelegate(DefaultDelegate):
     # Bluno Beetle uses cHandle 37
     def handleNotification(self, cHandle, data):
         try:
-            # Add incoming bytes to receive buffer
-            if len(data) < PACKET_SIZE:
-                print("{}Fragmented packet received{}".format(bcolors.BRIGHT_YELLOW, bcolors.ENDC))
-                self.fragmented_packet_count += 1
-            for dataByte in data:
-                if is_metadata_byte(dataByte) or len(self.dataBuffer) > 0:
-                    self.dataBuffer.append(dataByte)
-                else:
-                    print("Dropping byte {}".format(dataByte))
+            if cHandle == GATT_SERIAL_CHARACTERISTIC_HANDLE:
+                    # Add incoming bytes to receive buffer
+                if len(data) < PACKET_SIZE:
+                    print("{}Fragmented packet received{}".format(bcolors.BRIGHT_YELLOW, bcolors.ENDC))
+                    self.fragmented_packet_count += 1
+                for dataByte in data:
+                    if is_metadata_byte(dataByte) or len(self.dataBuffer) > 0:
+                        self.dataBuffer.append(dataByte)
+                    else:
+                        print("Dropping byte {}".format(dataByte))
+            else:
+                print(f"""{bcolors.BRIGHT_YELLOW}Bytes received from unknown handle {cHandle}, dropping{bcolors.ENDC}""")
         except Exception as exc:
             traceback.print_exception(exc)
 

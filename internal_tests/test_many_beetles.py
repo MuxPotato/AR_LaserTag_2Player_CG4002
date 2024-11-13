e455a848ed80474fca1bd13cb9359a581aea4baf
Wang Haoyang <haoyangw@gmail.com>
2024-11-13 20:54:58 +0800

    Update multiple-Beetle test code -Switch to enum for Beetle MAC addresses -Add individual queues per Beetle

--- internal_tests/test_many_beetles.py
+++ internal_tests/test_many_beetles.py
@@ -7,15 +7,18 @@ import threading
 import traceback
 
 from beetle import AnkleUnreliableBeetle, GloveUnreliableBeetle, GunBeetle, VestBeetle
-from internal_utils import GAME_STATE_QUEUE_TIMEOUT, GunUpdatePacket, VestUpdatePacket, bcolors
+from internal_utils import BEETLE_MAC_ADDR, GAME_STATE_QUEUE_TIMEOUT, INVALID_HP, GunUpdatePacket, VestUpdatePacket, bcolors
 
 class GameStateHandler(threading.Thread):
-    def __init__(self, incoming_game_state_queue: queue.Queue, gun_update_queue: queue.Queue, vest_update_queue: queue.Queue):
+    def __init__(self, incoming_game_state_queue: queue.Queue,
+            p1_gun_update_queue: queue.Queue, p1_vest_update_queue: queue.Queue,
+            p2_gun_update_queue: queue.Queue, p2_vest_update_queue: queue.Queue):
         super().__init__()
         self.incoming_game_state_queue = incoming_game_state_queue
-        # TODO: Add 2 queues(1 gun, 1 vest) per player
-        self.gun_update_queue = gun_update_queue
-        self.vest_update_queue = vest_update_queue
+        self.p1_gun_update_queue = p1_gun_update_queue
+        self.p1_vest_update_queue = p1_vest_update_queue
+        self.p2_gun_update_queue = p2_gun_update_queue
+        self.p2_vest_update_queue = p2_vest_update_queue
         self.stop_event = threading.Event()
 
     def quit(self):
@@ -33,16 +36,31 @@ class GameStateHandler(threading.Thread):
                 if new_game_state is None:
                     continue
                 # TODO: Add parser that checks player ID to determine which queue to put the update in
-                if self.is_gun_game_state(new_game_state):
-                    gun_update_packet = GunUpdatePacket(player_id = new_game_state.id, 
-                            bullets = new_game_state.bullets)
-                    # TODO: Check which player ID and put packet in the right Beetle's queue
-                    self.gun_update_queue.put(gun_update_packet)
-                elif self.is_vest_game_state(new_game_state):
-                    vest_update_packet = VestUpdatePacket(player_id = new_game_state.id, 
-                            is_hit = new_game_state.isShot, player_hp = new_game_state.hp)
-                    # TODO: Check which player ID and put packet in the right Beetle's queue
-                    self.vest_update_queue.put(vest_update_packet)
+                player_id = new_game_state["id"]
+                if self.has_gun_game_state(new_game_state):
+                    # TODO: Remove debug printing line below
+                    print(f"""{bcolors.BRIGHT_YELLOW}Gun game state update received{bcolors.ENDC}""")
+                    gun_update_packet = GunUpdatePacket(player_id = player_id,
+                            bullets = new_game_state["bullets"])
+                    if player_id == 1:
+                        self.p1_gun_update_queue.put(gun_update_packet)
+                    elif player_id == 2:
+                        self.p2_gun_update_queue.put(gun_update_packet)
+                    else:
+                        print(f"""{bcolors.BRIGHT_YELLOW}ERROR: Gun game state update received without player ID{bcolors.ENDC}""")
+                if self.has_vest_game_state(new_game_state):
+                    # TODO: Remove debug printing line below
+                    print(f"""{bcolors.BRIGHT_YELLOW}Vest game state update received{bcolors.ENDC}""")
+                    new_is_hit: bool = new_game_state["isHit"] if "isHit" in new_game_state.keys() else False
+                    new_hp: int = new_game_state["hp"] if "hp" in new_game_state.keys() else INVALID_HP
+                    vest_update_packet = VestUpdatePacket(player_id = player_id,
+                            is_hit = new_is_hit, player_hp = new_hp)
+                    if player_id == 1:
+                        self.p1_vest_update_queue.put(vest_update_packet)
+                    elif player_id == 2:
+                        self.p2_vest_update_queue.put(vest_update_packet)
+                    else:
+                        print(f"""{bcolors.BRIGHT_YELLOW}ERROR: Vest game state update received without player ID{bcolors.ENDC}""")
                 else:
                     print(f"""Unknown game state received from relay server: {new_game_state}""")
             except queue.Empty:
@@ -53,11 +71,11 @@ class GameStateHandler(threading.Thread):
             except Exception as exc:
                 traceback.print_exception(exc)
         
-    def is_gun_game_state(self, game_state: dict):
+    def has_gun_game_state(self, game_state: dict):
         return 'bullets' in game_state.keys()
     
-    def is_vest_game_state(self, game_state: dict):
-        return 'isShot' in game_state.keys()
+    def has_vest_game_state(self, game_state: dict):
+        return 'isHit' in game_state.keys() or 'hp' in game_state.keys()
 
 class TestBeetleThread(threading.Thread):
     MAIN_BLUNO_MAC_ADDR_LIST = [
@@ -85,20 +103,25 @@ class TestBeetleThread(threading.Thread):
 #        "B4:99:4C:89:18:1D",
     ]
 
-    def __init__(self, outgoing_ankle_queue, outgoing_glove_queue, outgoing_game_state_queue, incoming_game_state_queue):
+    def __init__(self, outgoing_p1_ankle_queue, outgoing_p1_glove_queue,
+                outgoing_p2_ankle_queue, outgoing_p2_glove_queue,
+                outgoing_game_state_queue, incoming_game_state_queue):
         super().__init__()
-        self.outgoing_ankle_queue = outgoing_ankle_queue
-        self.outgoing_glove_queue = outgoing_glove_queue
+        self.outgoing_p1_ankle_queue = outgoing_p1_ankle_queue
+        self.outgoing_p1_glove_queue = outgoing_p1_glove_queue
+        self.outgoing_p2_ankle_queue = outgoing_p2_ankle_queue
+        self.outgoing_p2_glove_queue = outgoing_p2_glove_queue
         self.outgoing_game_state_queue = outgoing_game_state_queue
         self.incoming_game_state_queue = incoming_game_state_queue
         self.incoming_glove_queue = queue.Queue()
-        # TODO: Add 1 gun queue per player
-        self.to_gun_queue = queue.Queue()
-        # TODO: Add 1 vest queue per player
-        self.to_vest_queue = queue.Queue()
+        self.p1_to_gun_queue = queue.Queue()
+        self.p1_to_vest_queue = queue.Queue()
+        self.p2_to_gun_queue = queue.Queue()
+        self.p2_to_vest_queue = queue.Queue()
         self.beetles = []
         self.game_state_handler = GameStateHandler(self.incoming_game_state_queue, 
-                self.to_gun_queue, self.to_vest_queue)
+                self.p1_to_gun_queue, self.p1_to_vest_queue,
+                self.p2_to_gun_queue, self.p2_to_vest_queue)
 
     def run(self):
         print("Starting Internal's main thread...")
@@ -113,54 +136,44 @@ class TestBeetleThread(threading.Thread):
         self.game_state_handler.join()
 
     def main(self):
-        colors = [bcolors.BRIGHT_RED, bcolors.BRIGHT_GREEN, bcolors.BRIGHT_BLUE, bcolors.BRIGHT_MAGENTA, bcolors.BRIGHT_CYAN, bcolors.BRIGHT_WHITE]
         try:
-            index = 0
-
-            """ beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            glove1Beetle = GloveUnreliableBeetle(beetle_addr, self.outgoing_glove_queue, self.incoming_glove_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P1_GLOVE.value
+            glove1Beetle = GloveUnreliableBeetle(beetle_addr, self.outgoing_p1_glove_queue, self.incoming_glove_queue, bcolors.BRIGHT_BLUE)
             self.beetles.append(glove1Beetle)
             glove1Beetle.start()
-            index += 1
 
-            beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            ankle1Beetle = AnkleUnreliableBeetle(beetle_addr, self.outgoing_ankle_queue, self.incoming_glove_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P1_ANKLE.value
+            ankle1Beetle = AnkleUnreliableBeetle(beetle_addr, self.outgoing_p1_ankle_queue, self.incoming_glove_queue, bcolors.BLUE)
             self.beetles.append(ankle1Beetle)
-            ankle1Beetle.start() 
-            index += 1"""
+            ankle1Beetle.start()
 
-            beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            gun1Beetle = GunBeetle(beetle_addr, self.outgoing_game_state_queue, self.to_gun_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P1_GUN.value
+            gun1Beetle = GunBeetle(beetle_addr, self.outgoing_game_state_queue, self.p1_to_gun_queue, bcolors.BRIGHT_RED)
             self.beetles.append(gun1Beetle)
             gun1Beetle.start()
-            index += 1
 
-            beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            vest1Beetle = VestBeetle(beetle_addr, self.outgoing_game_state_queue, self.to_vest_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P1_VEST.value
+            vest1Beetle = VestBeetle(beetle_addr, self.outgoing_game_state_queue, self.p1_to_vest_queue, bcolors.BRIGHT_CYAN)
             self.beetles.append(vest1Beetle)
             vest1Beetle.start()
-            index += 1
 
-            beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            glove2Beetle = GloveUnreliableBeetle(beetle_addr, self.outgoing_glove_queue, self.incoming_glove_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P2_GLOVE.value
+            glove2Beetle = GloveUnreliableBeetle(beetle_addr, self.outgoing_p2_glove_queue, self.incoming_glove_queue, bcolors.BRIGHT_MAGENTA)
             self.beetles.append(glove2Beetle)
             glove2Beetle.start()
-            index += 1
 
-            beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            ankle2Beetle = AnkleUnreliableBeetle(beetle_addr, self.outgoing_ankle_queue, self.incoming_glove_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P2_ANKLE.value
+            ankle2Beetle = AnkleUnreliableBeetle(beetle_addr, self.outgoing_p2_ankle_queue, self.incoming_glove_queue, bcolors.MAGENTA)
             self.beetles.append(ankle2Beetle)
             ankle2Beetle.start()
-            index += 1
 
-            beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            gun2Beetle = GunBeetle(beetle_addr, self.outgoing_game_state_queue, self.to_gun_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P2_GUN.value
+            gun2Beetle = GunBeetle(beetle_addr, self.outgoing_game_state_queue, self.p2_to_gun_queue, bcolors.BRIGHT_GREEN)
             self.beetles.append(gun2Beetle)
             gun2Beetle.start()
-            index += 1
 
-            beetle_addr = TestBeetleThread.MAIN_BLUNO_MAC_ADDR_LIST[index]
-            vest2Beetle = VestBeetle(beetle_addr, self.outgoing_game_state_queue, self.to_vest_queue, colors[index])
+            beetle_addr = BEETLE_MAC_ADDR.P2_VEST.value
+            vest2Beetle = VestBeetle(beetle_addr, self.outgoing_game_state_queue, self.p2_to_vest_queue, bcolors.BRIGHT_WHITE)
             self.beetles.append(vest2Beetle)
             vest2Beetle.start()
 
@@ -177,11 +190,13 @@ class TestBeetleThread(threading.Thread):
             traceback.print_exception(exc)
 
 if __name__=="__main__":
-    outgoing_ankle_queue = queue.Queue()
-    outgoing_glove_queue = queue.Queue()
+    outgoing_p1_ankle_queue = queue.Queue()
+    outgoing_p1_glove_queue = queue.Queue()
+    outgoing_p2_ankle_queue = queue.Queue()
+    outgoing_p2_glove_queue = queue.Queue()
     outgoing_game_state_queue = queue.Queue()
     incoming_game_state_queue = queue.Queue()
-    test_thread = TestBeetleThread(outgoing_ankle_queue,outgoing_glove_queue,outgoing_game_state_queue,incoming_game_state_queue)
+    test_thread = TestBeetleThread(outgoing_p1_ankle_queue,outgoing_p1_glove_queue,outgoing_p2_ankle_queue,outgoing_p2_glove_queue,outgoing_game_state_queue,incoming_game_state_queue)
 
     try:
         test_thread.start()
@@ -191,8 +206,8 @@ if __name__=="__main__":
         test_thread.quit()
         test_thread.join()
         time.sleep(1)
-        print(f"""Number of elements in outgoing glove queue: {outgoing_glove_queue.qsize()}""")
-        print(f"""Number of elements in outgoing game state queue: {outgoing_game_state_queue.qsize()}""")
+        #print(f"""Number of elements in outgoing glove queue: {outgoing_glove_queue.qsize()}""")
+        #print(f"""Number of elements in outgoing game state queue: {outgoing_game_state_queue.qsize()}""")
     except Exception as exc:
         traceback.print_exception(exc)
     sys.exit(0)
